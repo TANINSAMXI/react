70ac7b7beb0c876cb1a71ff8b4880ea02c376f32
TANINSAMXI <pischaniyd@gmail.com>
2025-01-26 10:45:40 +0100

    hw_55 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import MyForm from "./components/MyForm";
+import "./css/styles.css";
+function App() {
+  return (
+    <div className="App">
+      <MyForm />
+    </div>
+  );
+}
 
 export default App;
