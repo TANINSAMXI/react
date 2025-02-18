8a6971e632a37593d2da33fc17dd33fa9ed41b8d
TANINSAMXI <pischaniyd@gmail.com>
2025-02-18 13:13:40 +0100

    hw_58 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-function App() {}
+import { Container } from "react-bootstrap";
+import OrdersPage from "./pages/OrdersPage";
+
+const App = () => (
+  <Container>
+    <OrdersPage />
+  </Container>
+);
 
 export default App;
